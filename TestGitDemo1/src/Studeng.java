808c81bd24b9bac0fbc18cbf7487ef62e81920d8
fengl <598932833@qq.com>
2021-10-25 15:51:38 +0800

    Studeng中添加了age,name属性

---
+++ TestGitDemo1/src/Studeng.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Fengl
+ * @Date: 2021/10/25-10-25-下午3:50
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Studeng {
+    private int age;
+    private String name;
+}
